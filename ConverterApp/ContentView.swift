57cc75b6e3f7134d8613986e352ce09407bbef12
Timmy Le <le.timmy94@gmail.com>
2025-06-16 17:47:03 -0400

    Initial Commit

---
+++ ConverterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConverterApp
+//
+//  Created by Phuc Le on 6/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ConverterApp/ConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterAppApp.swift
+//  ConverterApp
+//
+//  Created by Phuc Le on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
