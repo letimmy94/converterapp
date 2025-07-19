d89cd249cbe727deca69868738759a1a149e60f1
Timmy Le <le.timmy94@gmail.com>
2025-07-19 00:48:15 -0400

    more stuff

--- ConverterApp/ContentView.swift
+++ ConverterApp/ContentView.swift
@@ -16,30 +16,53 @@ struct ContentView: View {
                 .ignoresSafeArea()
             VStack {
                 // prancing pony image
-                
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 // currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 HStack {
                     // Left conversion section
                     VStack {
                         // currency
                         HStack {
-                            
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            Text("Silverpiece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         // text field
+                        Text("Text Field")
                     }
                     // Equal sign
-                    
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     // Right conversion section
                     VStack {
                         // currency
                         HStack {
-                            
-                        }
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)                        }
                         // text field
+                        Text("Text Field")
                     }                }
                 // conversion section
-                
+                Spacer()
             }
+//            .border(.red)
         }
     }
 }
